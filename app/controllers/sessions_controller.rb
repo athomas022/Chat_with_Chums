1608c06a998f24aaf0565c7418399da4186ee665
Aleena Thomas <aleenatom92@gmail.com>
2024-06-04 15:14:32 -0500

    rails user authentication in progress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,4 +7,13 @@ class SessionsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def generate_jwt(user)
+    JWT.encode({ id: user.id, exp: 60.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
+  end
+
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    devise :database_authenticatable, :registerable
     has_many :participants
     has_many :chat_rooms, through: :participants
     has_many :create_chat_rooms, class_name: 'ChatRoom', foreign_key: 'admin_id'
