0d51c1a0da2114c43ec4b940fa7cde2255a8fa65
Aleena Thomas <aleenatom92@gmail.com>
2024-06-07 09:06:38 -0500

    backend for the chat channel messaging is done

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,9 +1,8 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream from "chat_room_#{params[:chat_room_id]}"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,21 @@
 class Message < ApplicationRecord
     belongs_to :user, class_name: 'User', foreign_key: 'user_id'
     belongs_to :chat_room, class_name: 'ChatRoom', foreign_key: 'chat_room_id'
+
+after_create_commit {broadcast_message}
+
+private
+def broadcast_message
+    username = user.name if user
+    ActionCable.server.broadcast("chat_room#{chat_room_id}"  , {
+      id: id,
+      body: body,
+      user_id: user_id,
+      username: username,
+      message_html: render_message   
+    })
 end
+
+def render_message
+    ApplicationController.render(partial: 'messages/message', locals: { message: self })
+  end
