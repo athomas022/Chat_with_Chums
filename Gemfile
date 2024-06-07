f4e3a4da64421b4f78c73fa1af83c7660ce2d537
Aleena Thomas <aleenatom92@gmail.com>
2024-06-07 08:01:30 -0500

    started the webSockets API build

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ gem 'devise-jwt'
 
 gem 'foreman'
 
+gem 'rack-cors'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,31 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+      def find_verified_user
+        token = request.headers[:HTTP_AUTHORIZATION]
+        if token.present?
+          decoded_token = decode_jwt(token)
+          if decoded_token && (user_id = decoded_token["id"])
+            return User.find_by(id: user_id)
+          end
+        else
+          reject_unauthorized_connection
+        end
+      end
+      
+        def decode_jwt(token)
+          begin
+            JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS256').first
+          rescue JWT::DecodeError
+            nil
+          end
+        end
+      end
   end
-end
+
